6c2979b1fa99f172f0ee64c2fe26e59f90b8dfe7
hygsani <blek.wind@gmail.com>
2020-05-28 11:12:44 +0700

    rename container file with timestamps

--- backup.py
+++ backup.py
@@ -1,16 +1,23 @@
 import subprocess
+from datetime import datetime
+
+def format_datetime(now):
+    return now.strftime('%y%m%d_%H%M%S')
 
 def create_file(curr_timestamps):
-    open('containers_200528', 'w')
+    open('containers_' + curr_timestamps, 'w')
 
-def write_file(curr_timestamps):
-    f = open('containers_200528', 'w')
-    f.write('hello world')
+def write_file(curr_timestamps, content):
+    f = open('containers_' + curr_timestamps, 'w')
+    f.write(content)
     f.close()
 
 def execute_shell(cmd):
     return subprocess.check_output(cmd, universal_newlines=True)
 
 ps = execute_shell(['docker', 'ps', '--format', '{{.ID}}|{{.Image}}|{{.Ports}}'])
-create_file()
-write_file()
+
+timestamps = format_datetime(datetime.now())
+
+create_file(timestamps)
+write_file(timestamps, ps)
